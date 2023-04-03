bc0d5f0231e0598bce13905fe1a431937d136ae9
Nachiket <patelnachiket828@gmail.com>
2023-04-03 10:02:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-name = input("Enter Your Name : ")
-print ("Number is : "+name)
+#float calculator
+x = float(input("What is x ? : "))
+y = float(input("What is y ? : "))
+z = round(x + y) # it will round the figure
+z = round(x / y ,3) # it will cutdown whole number with last 3 decimal
+print (f"{z:.2f}")
+print (f"{z:,}")
+
+# Creating our Own function
+def main():
+name_your = input("What is Your name ? : ")
+hello(name_your)
+
+def hello(to="Coders") :
+    print("Hello," , to)
+    
+main()
+
+# square program with function
+def main():
+  x = int(input("Enter x : "))
+  print("Square of x is : " , square(x))
+      
+def square(n):
+  return n * n
+    
+main()
