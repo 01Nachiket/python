db1ee5bc2376b0ff8345cd57788f08265464efa9
Nachiket Patel <patelnachiket828@gmail.com>
2023-04-06 22:32:05 +0530

    string case change function

---
+++ cases.py
@@ -0,0 +1,7 @@
+user_input = input("Enter String in Capital Letter's : ").lower()
+#print("-! This will only work when you give uppercase string")
+print("OUTPUT >>> " + user_input)
+
+user_input = input("Enter String in Small Alphabet letter's  : ").upper()
+#print("-! This will only work when you give uppercase string")
+print("OUTPUT >>> " + user_input)
