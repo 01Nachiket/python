551f6ae6dc6d7c6b4a31b44ef35dca9b300b5c77
Nachiket Patel <patelnachiket828@gmail.com>
2023-04-10 09:00:23 +0530

    .title_Function

--- cases.py
+++ cases.py
@@ -3,5 +3,9 @@ user_input = input("Enter String in Capital Letter's : ").lower()
 print("OUTPUT >>> " + user_input)
 
 user_input = input("Enter String in Small Alphabet letter's  : ").upper()
-#print("-! This will only work when you give uppercase string")
+#print("-! This will only work when you give string in lower case")
+print("OUTPUT >>> " + user_input)
+
+user_input = input("Enter String in Small Alphabet letter's  : ").title()
+#print("-! This function will convert string into camel case ")
 print("OUTPUT >>> " + user_input)
